1f18638afb2a59aedd9add6d85d8708dca25b681
wrathdragonflair <milars20@student.aau.dk>
2020-10-22 01:29:06 +0200

    Lavede Scan Data funktion færdig

--- IMPR/Eksamen/Eksamen1.c
+++ IMPR/Eksamen/Eksamen1.c
@@ -3,10 +3,13 @@
 void scan_data(operator, operand);
 void do_next_op(operator, operand);
 double run_calculator();
+int isBinary(operator);
 
 int main(void)
 {
-    double akkumulator;
+    double akkumulator, operand;
+    char operator;
+
 
     akkumulator = 0.0;
 
@@ -16,15 +19,32 @@ int main(void)
 
 void scan_data(operator, operand)
 {
+    scanf(" %c", &operator);
 
+    if(isBinary(operator) = 1)
+    {
+        scanf(" %lf", &operand);
+    }
+    else
+    {
+        operand = 0.0;
+    }
 }
 
 void do_next_op(operator, operand)
 {
 
+
 }
 
 double run_calculator()
 {
+
+}
+
+int isBinary(operator)
+{
+    if()
+
 
 }
