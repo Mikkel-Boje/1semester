77e6b0b23b90fa47807a3f094582390c50dc1db7
Mikkel <milars20@student.aau.dk>
2020-10-21 14:19:32 +0200

    Startede på eksamen

---
+++ IMPR/Eksamen/Eksamen1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void scan_data(operator, operand);
+void do_next_op(operator, operand);
+double run_calculator();
+
+int main(void)
+{
+    double akkumulator;
+
+    akkumulator = 0.0;
+
+
+
+}
+
+void scan_data(operator, operand)
+{
+
+}
+
+void do_next_op(operator, operand)
+{
+
+}
+
+double run_calculator()
+{
+
+}
