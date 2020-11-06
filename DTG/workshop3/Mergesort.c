1315bf185b467327be4faac96898c297f68366f0
wrathdragonflair <milars20@student.aau.dk>
2020-11-06 12:26:01 +0100

    Started workshop 3

---
+++ DTG/workshop3/Mergesort.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+void Merge(int L[], int start, int slut, int midt);
+void MergeSort(int L[], int start, int slut);
+void printList(int L[], int size);
+
+int main(void){ 
+  int L[] = { 5, 3, 8, 1, 6, 10, 7, 2, 4, 9 }; 
+  int L_size = sizeof(L) / sizeof(L[0]); 
+  	
+  printf("Givet listen \n"); 
+  printList(L, L_size); 
+  
+  MergeSort(L, 0, L_size - 1); 
+  
+  printf("Er den sorterede liste \n"); 
+  printList(L, L_size); 
+  return 0; 
+} 
+
+void Merge(int L[], int start, int slut, int midt){
+  /* Start med at kopiere de to dellister ind i to nye lister L1 og L2, så vi ikke sletter nogle elementer fra L når vi begynder at flette elementerne derind */
+  
+  /* Herefter skal vi kopiere elementer fra L1 og L2 ind på pladserne i L mellem start og slut men i sorteret rækkefølge */
+	
+}
+
+void MergeSort(int L[], int start, int slut){
+  /* Udfyld her */
+}
+
+void printList(int L[], int size){ 
+  int i; 
+  for (i = 0; i < size; i++) 
+    printf("%d ", L[i]); 
+  printf("\n"); 
+} 
